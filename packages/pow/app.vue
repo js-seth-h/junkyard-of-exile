b8ae6ef823e413ff6ffc1ee86b0ffe33fe6ee053
js <js.seth.h@gmail.com>
2021-06-04 19:42:53 +0900

    프로젝트 초기화

---
+++ packages/pow/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    asdfsadf
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'App',
+    data(){
+      return{}
+    }
+  }
+</script>
+
+
+<style lang="scss">
+</style>
