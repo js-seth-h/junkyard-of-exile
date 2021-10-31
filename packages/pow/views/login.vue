57e090f6da2dca3cd3ab3683d0c3475e2267e22e
ejkim <ugoo2457@gmail.com>
2021-10-31 17:42:29 +0900

    * 에러 - 페이트론 로그인

--- packages/pow/views/login.vue
+++ packages/pow/views/login.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <div> this is login page</div>
-    <button @click="login">login btn</button>
+    <button @click="login">goto login</button>
+    <br />
+    <a
+      href="https://patreon.com/oauth2/authorize?response_type=code&amp;client_id=GNLVthqCf49GAa9jaDbGZioqk3xvz0QoAcPkWKR3WbI5087O1Um68INjPTLVd5kQ&amp;redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fpatreon%2Fredirect&amp;state=chill">Login
+      with Patreon</a>
   </div>
 </template>
 
